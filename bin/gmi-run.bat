e01ae50eeb723ef094c27204e866068e90843b65
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2009-11-16 08:27:44 +0000

    Updated scripts

--- bin/gmi-run.bat
+++ bin/gmi-run.bat
@@ -6,9 +6,6 @@ rem %~dp0 is expanded pathname of the current script under NT
 
 if "%GMI_HOME%X"=="X" set GMI_HOME=%~dp0..
 
-set JAVACLASSPATH=%CLASSPATH%;
-for %%i in ("%GMI_HOME%\lib\*.jar") do call "%GMI_HOME%\bin\AddToGmiClassPath.bat" %%i
-
 set GMI_APP_ARGS=
 
 :setupArgs
@@ -19,6 +16,6 @@ goto setupArgs
 
 :doneStart
 
-java -classpath "%JAVACLASSPATH%" -Dlog4j.configuration=file:"%GMI_HOME%"\log4j.properties %GMI_APP_ARGS%
+java -classpath "%CLASSPATH%;%GMI_HOME\lib\*" -Dlog4j.configuration=file:"%GMI_HOME%"\log4j.properties %GMI_APP_ARGS%
 
 if "%OS%"=="Windows_NT" @endlocal
--- bin/gmic.bat
+++ bin/gmic.bat
@@ -6,9 +6,6 @@ rem %~dp0 is expanded pathname of the current script under NT
 
 if "%GMI_HOME%X"=="X" set GMI_HOME=%~dp0..
 
-set JAVACLASSPATH=%CLASSPATH%;
-for %%i in ("%GMI_HOME%\lib\*.jar") do call "%GMI_HOME%\bin\AddToGmiClassPath.bat" %%i
-
 set IBISC_ARGS=
 
 :setupArgs
@@ -19,7 +16,7 @@ goto setupArgs
 
 :doneStart
 
-java -classpath "%JAVACLASSPATH%" -Dlog4j.configuration=file:"%GMI_HOME%"\log4j.properties ibis.compile.Ibisc -gmi %IBISC_ARGS%
+java -classpath "%JAVACLASSPATH%;%GMI_HOME%\lib\*" -Dlog4j.configuration=file:"%GMI_HOME%"\log4j.properties ibis.compile.Ibisc -gmi %IBISC_ARGS%
 
 if "%OS%"=="Windows_NT" @endlocal
 
